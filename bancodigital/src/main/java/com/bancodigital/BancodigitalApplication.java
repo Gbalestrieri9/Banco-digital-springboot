489ed26615518ad06941748070bceb4d62d79e84
gabriel barros <gabrielbalestrieri@hotmail.com>
2024-04-02 22:41:29 -0300

    Projeto iniciado

---
+++ bancodigital/src/main/java/com/bancodigital/BancodigitalApplication.java
@@ -0,0 +1,14 @@
+package com.bancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodigitalApplication.class, args);
+		System.out.println("Hello word!");
+	}
+
+}
